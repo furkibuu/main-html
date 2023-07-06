08f47894089d639a87936db298cbdefcc7af341f
Furki <74312970+fu-w@users.noreply.github.com>
2023-07-06 10:19:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+let elementOutofView = (el) => {
+  let elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+// Come Back Site Name
+
+const name1 = document.title;
+        window.onblur = () =>
+        document.title = "Come Back Please :(";
+        window.onfocus = () =>
+        document.title = name1;
+
